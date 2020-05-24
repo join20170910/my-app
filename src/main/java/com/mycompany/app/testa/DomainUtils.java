6eead578433e5c5dd943d224973394def3137723
john <wyj8574@163.com>
2020-05-24 13:33:44 +0800

    添加

--- src/main/java/com/mycompany/app/testa/DomainUtils.java
+++ src/main/java/com/mycompany/app/testa/DomainUtils.java
@@ -2,6 +2,9 @@ package com.mycompany.app.testa;
 
 import java.util.regex.Pattern;
 
+/**
+ * @author john
+ */
 public class DomainUtils {
 
     private static final String DOMAIN_NAME_PATTERN = "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$";
