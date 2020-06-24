9591cd127860772d4325ed36cc5e5df5e55e59c5
wyj01 <wyj01@163.com>
2020-06-24 23:41:07 +0800

    Oauth2 改为动态参数

--- is-server-auth/src/main/java/com/mycompany/app/server/auth/config/UserDetailsServiceImpl.java
+++ is-server-auth/src/main/java/com/mycompany/app/server/auth/config/UserDetailsServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 
 /**
@@ -26,7 +25,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        return User.withUsername("user")
+        return User.withUsername(username)
                 .password(passwordEncoder.encode("123456"))
                 .authorities("ROLE_ADMIN")
                 .build();
