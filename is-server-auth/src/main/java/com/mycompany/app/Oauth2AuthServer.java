f7eb70b2fddea4e059b855db8abde87a64610714
wyj01 <wyj01@163.com>
2020-06-28 21:56:24 +0800

    <Oauth2 增加认证> 自定义logoutpage 界面，登出后重定向到指定界面

--- is-server-auth/src/main/java/com/mycompany/app/Oauth2AuthServer.java
+++ is-server-auth/src/main/java/com/mycompany/app/Oauth2AuthServer.java
@@ -3,6 +3,9 @@ package com.mycompany.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author john
+ */
 @SpringBootApplication
 public class Oauth2AuthServer {
   public static void main(String[] args) {
