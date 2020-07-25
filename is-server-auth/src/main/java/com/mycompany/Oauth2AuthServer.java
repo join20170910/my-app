cb6b248bbe88071fc49888843cc3cea6c24e4d6b
wyj01 <wyj01@163.com>
2020-07-25 16:57:02 +0800

    基础功能

--- is-server-auth/src/main/java/com/mycompany/Oauth2AuthServer.java
+++ is-server-auth/src/main/java/com/mycompany/Oauth2AuthServer.java
@@ -3,12 +3,15 @@ package com.mycompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author john
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+
 public class Oauth2AuthServer {
   public static void main(String[] args) {
       SpringApplication.run(Oauth2AuthServer.class,args);
